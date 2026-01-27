bde0186128b55fcd967c40acf9e98e3e0c3f7cc2
KylanDelpouve <kylan.delpouve@univ-rouen.fr>
2026-01-27 09:44:30 +0100

    Mise à jour fichier init_db.sql
    Modification des fichiers :
    - init_db.sql
    - Dockerfile (dans Postgres)
    - docker-compose.yml

--- radius/init_db.sql
+++ radius/init_db.sql
@@ -1,1 +1,12 @@
+-- Création de la table pour les utilisateurs
+CREATE TABLE radcheck (
+    id serial PRIMARY KEY,
+    username text NOT NULL,
+    attribute text NOT NULL,
+    op varchar(2) NOT NULL DEFAULT '==',
+    value text NOT NULL
+);
 
+-- Ajout de l'utilisateur Bob pour le test
+INSERT INTO radcheck (username, attribute, op, value) 
+VALUES ('bob', 'Cleartext-Password', ':=', 'hello');
